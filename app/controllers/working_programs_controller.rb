e2cae4e3809a41bf8e571a588066a7c13e5c9de4
Alina Moskieva <alina.moskieva@flatstack.com>
2020-04-11 00:17:00 +0200

    Add ability to create working program

---
+++ app/controllers/working_programs_controller.rb
@@ -0,0 +1,36 @@
+class WorkingProgramsController < ApplicationController
+  expose :working_program
+  expose :working_programs, ->{ discipline.working_programs }
+
+  expose :discipline
+
+  def create
+    working_program.discipline = discipline
+
+    if working_program.save
+      redirect_to working_program, notice: "Working program was successfully created."
+    else
+      render :new
+    end
+  end
+
+  def update
+    if working_program.update(working_program_params)
+      redirect_to working_program, notice: "Working program was successfully updated."
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    working_program.destroy
+    redirect_to working_program.discipline, notice: "Working program was successfully destroyed."
+  end
+
+  private
+
+  def working_program_params
+    params.require(:working_program).permit(:start_at, :developed_at, :developed_in, :language,
+      :adapt_to_needs_of_disabilities)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   resources :educational_institutions, only: %i(index new create update show edit destroy) do
     resources :training_directions, shallow: true do
-      resources :disciplines, shallow: true
+      resources :disciplines, shallow: true do
+        resources :working_programs, shallow: true
+      end
     end
   end
 

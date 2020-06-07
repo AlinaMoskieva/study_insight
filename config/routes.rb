c68f74e4099dff5f4619b6f5b06c232787419b1a
Alina Moskieva <alina.moskieva@flatstack.com>
2020-06-07 17:38:59 +0200

    Add page for retrospective analytics

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :universities, only: %i(index new create update show edit destroy) do
     resources :institutions, shallow: true do
       resources :departments, shallow: true do
+        resources :retrospective_analytics, only: :index
         resources :curriculums, shallow: true do
           resources :disciplines, shallow: true do
             resources :discipline_programs, shallow: true
